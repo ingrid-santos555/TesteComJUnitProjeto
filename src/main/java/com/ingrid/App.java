6122ecee6b0e84a91c9bd3a18c7d461c26954b98
ingrid-santos555 <ingridtayne19@gmail.com>
2025-04-09 19:51:59 -0300

    Add files via upload

---
+++ src/main/java/com/ingrid/App.java
@@ -0,0 +1,13 @@
+package com.ingrid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
